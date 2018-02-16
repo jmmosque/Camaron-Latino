51dae0ffa96552f9eef6f2ecf8c952919e61dcf4
cmoncayo <cmoncayo@espol.edu.ec>
2018-02-16 11:25:31 -0500

    e

--- indexadministrativo.php
+++ indexadministrativo.php
@@ -174,7 +174,8 @@ session_start();
     <script src="js/sb-admin.min.js"></script>
     <!-- Custom scripts for this page-->
     <script src="js/sb-admin-datatables.min.js"></script>
-    <script src="js/sb-admin-charts.min.js"></script>  </div>
+    <script src="js/sb-admin-charts.min.js"></script>  
+    </div>
 </body>
 
 </html>
