f0412c03d202b9d8cf0ff6b6655407d5bea85fdb
cmoncayo <cmoncayo@espol.edu.ec>
2018-02-16 11:36:43 -0500

    m

--- Collector.php
+++ Collector.php
@@ -12,7 +12,7 @@ define("DB_HOST", "ec2-54-227-243-210.compute-1.amazonaws.com");
 define("DB_USER", "apxqfsaxnvgzic");
 define("DB_PASS", "8083764562884e01c15282ea6c5c1274e754a0d8dff6d8d2b0af63504d3be430");
 define("DB_NAME", "dh9sa2bsn68st");
-
+//
 class Collector extends dataBase
 {
   public static $db;
