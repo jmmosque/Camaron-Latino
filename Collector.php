d9dfc28222e6156011c6c7f8615b52edd1beccd7
davidr123 <davidradaalejandro@gmail.com>
2018-02-17 16:58:14 -0500

    dhsufh

--- Collector.php
+++ Collector.php
@@ -11,7 +11,7 @@ include_once('dataBase.php');
 
 
 
-define("DB_HOST", "ec2-54-83-37-223.compute-1.amazonaws.com");
+define("DB_HOST", "ec2-54-227-243-210.compute-1.amazonaws.com");
 define("DB_USER", "apxqfsaxnvgzic");
 define("DB_PASS", "8083764562884e01c15282ea6c5c1274e754a0d8dff6d8d2b0af63504d3be430");
 define("DB_NAME", "dh9sa2bsn68st");
